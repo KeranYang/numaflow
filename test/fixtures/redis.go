4b3d7764d9753c6c2b35996a9d31522e345e32e8
Keran Yang <yangkr920208@gmail.com>
2022-12-01 10:32:30 -0500

    Test - 1
    Signed-off-by: Keran Yang <yangkr920208@gmail.com>

---
+++ test/fixtures/redis.go
@@ -0,0 +1,47 @@
+/*
+Copyright 2022 The Numaproj Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package fixtures
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+)
+
+func GetRedisString(key string) string {
+	str := InvokeE2EAPI("/redis/get-string?key=%s", key)
+	return str
+}
+
+func ExpectRedisKeyValue(key string, value string, timeout time.Duration) {
+	log.Printf("expecting key value pair in redis: key %s, value %s within %v\n", key, value, timeout)
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+	for {
+		select {
+		case <-ctx.Done():
+			panic(fmt.Errorf("timeout waiting"))
+		default:
+			valueInRedis := GetRedisString(key)
+			if valueInRedis == value {
+				return
+			}
+			time.Sleep(time.Second)
+		}
+	}
+}
