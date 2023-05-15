9e06caf94c2bc0b65e0146894c1c86819c52a5fe
Keran Yang <yangkr920208@gmail.com>
2023-05-15 11:06:07 -0400

    NOT STABLE, daemon pod periodically crashes and NOT ACCURATE, sometimes the rate are way higher than expected
    Signed-off-by: Keran Yang <yangkr920208@gmail.com>

--- pkg/daemon/server/service/rate_v2/optimized_rate_calculator.go
+++ pkg/daemon/server/service/rate_v2/optimized_rate_calculator.go
@@ -27,6 +27,9 @@ import (
 	sharedqueue "github.com/numaproj/numaflow/pkg/shared/queue"
 )
 
+// fixedLookbackSeconds Always maintain rate metrics for the following lookback seconds (1m, 5m, 15m)
+var fixedLookbackSeconds = map[string]int64{"1m": 60, "5m": 300, "15m": 900}
+
 // TimestampedCount is a helper struct to wrap a count number and timestamp pair
 type TimestampedCount struct {
 	// count is the number of messages processed
@@ -86,17 +89,63 @@ func (orc *OptimizedRateCalculator) Start(ctx context.Context) error {
 			case <-ctx.Done():
 				return
 			case <-ticker.C:
-				// TODO - implement it
-				// update the timestampedTotalCounts map
+				newLatestSawTime := time.Now().Unix()
+				orc.log.Infof("Start updating the timestamped total counts...")
+				for _, v := range orc.pipeline.Spec.Vertices {
+					// assuming countTracker, podTracker and rateTracker start at the same time approximately.
+					// so not really using the timestamp in the countTracker yet.
+
+					// get the latest count from the count tracker
+					c := orc.countTracker.GetCounts(v.Name)
+					delta := 0.0
+					for _, v := range c {
+						if v.latestSawCount.count == CountNotAvailable {
+							continue
+						}
+						if v.prevSawCount.count == CountNotAvailable || v.latestSawCount.count < v.prevSawCount.count {
+							delta += v.latestSawCount.count
+							continue
+						}
+						delta += v.latestSawCount.count - v.prevSawCount.count
+					}
+
+					orc.log.Infof("Vertex %s has delta %f", v.Name, delta)
+
+					if delta == 0 && orc.timestampedTotalCounts[v.Name].Length() == 0 {
+						// if delta is 0 and the queue is empty, we don't need to update the queue
+						// because the pipeline is still collecting data.
+						// this also prevent the issue of getting unrealistic high rates when the pipeline is just started.
+						continue
+					}
+
+					var newTotalCount float64
+					if orc.timestampedTotalCounts[v.Name].Length() == 0 {
+						newTotalCount = delta
+					} else {
+						newTotalCount = orc.timestampedTotalCounts[v.Name].Newest().count + delta
+					}
+					orc.timestampedTotalCounts[v.Name].Append(TimestampedCount{count: newTotalCount, timestamp: newLatestSawTime})
+				}
+				orc.log.Infof("Finished updating the timestamped total counts")
+				for k, v := range orc.timestampedTotalCounts {
+					for _, i := range v.Items() {
+						orc.log.Infof("Vertex %s has timestamped total count %f at timestamp %d", k, i.count, i.timestamp)
+					}
+				}
 			}
 		}
 	}()
 	return nil
 }
 
-// GetRates returns the processing rates of the vertex for the given lookback seconds.
+// GetRates returns the processing rates of the vertex for the given lookback seconds (1, 5, and 15m).
 func (orc *OptimizedRateCalculator) GetRates(vn string) map[string]float64 {
-	// TODO - implement it
-	_ = vn
-	return nil
+	res := make(map[string]float64)
+	// calculate rates for each lookback seconds
+	// TODO - add user specified lookback seconds
+	for n, i := range fixedLookbackSeconds {
+		r := CalculateRate(orc.timestampedTotalCounts[vn], i)
+		res[n] = r
+	}
+	return res
 }
