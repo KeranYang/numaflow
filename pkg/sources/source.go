338ec6fd309759aab1e75f992a07e2680978dbf8
Keran Yang <yangkr920208@gmail.com>
2024-02-15 18:19:09 -0500

    fail transformer
    Signed-off-by: Keran Yang <yangkr920208@gmail.com>

--- pkg/sources/source.go
+++ pkg/sources/source.go
@@ -214,7 +214,7 @@ func (sp *SourceProcessor) Start(ctx context.Context) error {
 	maxMessageSize := sharedutil.LookupEnvIntOr(dfv1.EnvGRPCMaxMessageSize, sdkclient.DefaultGRPCMaxMessageSize)
 	if sp.VertexInstance.Vertex.HasUDTransformer() {
 		// Wait for server info to be ready
-		serverInfo, err := sdkserverinfo.SDKServerInfo()
+		serverInfo, err := sdkserverinfo.SDKServerInfo(sdkserverinfo.WithServerInfoFilePath("/var/run/numaflow/sourcetransformer-server-info"))
 		if err != nil {
 			return err
 		}
