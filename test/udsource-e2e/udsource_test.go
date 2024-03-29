e31f4aebe157e6242e584c5d6901fe1faf054018
Keran Yang <yangkr920208@gmail.com>
2024-03-29 17:59:08 -0400

    .
    Signed-off-by: Keran Yang <yangkr920208@gmail.com>

--- test/udsource-e2e/udsource_test.go
+++ test/udsource-e2e/udsource_test.go
@@ -19,12 +19,18 @@ limitations under the License.
 package e2e
 
 import (
+	"context"
 	"fmt"
+	"math"
 	"sync"
 	"testing"
+	"time"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 
+	dfv1 "github.com/numaproj/numaflow/pkg/apis/numaflow/v1alpha1"
+	daemonclient "github.com/numaproj/numaflow/pkg/daemon/client"
 	. "github.com/numaproj/numaflow/test/fixtures"
 )
 
@@ -68,8 +74,13 @@ func (s *UserDefinedSourceSuite) testSimpleSource(lang string) {
 		CreatePipelineAndWait()
 	defer w.DeletePipelineAndWait()
 
-	// wait for all the pods to come up
-	w.Expect().VertexPodsRunning()
+	pipelineName := fmt.Sprintf("simple-source-%s", lang)
+
+	// wait for all the pods and daemon server to come up
+	w.Expect().VertexPodsRunning().DaemonPodsRunning().DaemonPodLogContains(pipelineName, LogDaemonStarted)
+	// port-forward daemon server
+	defer w.DaemonPodPortForward(pipelineName, 1234, dfv1.DaemonServicePort).
+		TerminateAllPodPortForwards()
 
 	// we use the log sink instead of redis to verify the output because the simple user-defined source generates
 	// such a large amount of data that the redis sink is not able to handle it, it breaks with OOM error
@@ -84,6 +95,60 @@ func (s *UserDefinedSourceSuite) testSimpleSource(lang string) {
 	w.Expect().VertexPodLogContains("out", "630")
 	w.Expect().VertexPodLogContains("out", "999")
 
+	// verify the processing rate match between source and sink
+	client, err := daemonclient.NewDaemonServiceClient("localhost:1234")
+	assert.NoError(s.T(), err)
+	defer func() {
+		_ = client.Close()
+	}()
+
+	// timeout the test if rates don't match in 1 minute.
+	timer := time.NewTimer(60 * time.Second)
+	// we use 10-second windows for rate calculation
+	// wait for 10 seconds for a new timestamped count entry to be added to the rate calculation windows
+	waitInterval := 10 * time.Second
+	succeedChan := make(chan struct{})
+	go func() {
+		vertexNames := []string{"in", "out"}
+		for {
+			var rates []float64
+			for _, vertexName := range vertexNames {
+				m, err := client.GetVertexMetrics(context.Background(), pipelineName, vertexName)
+				assert.NoError(s.T(), err)
+				assert.Equal(s.T(), pipelineName, *m[0].Pipeline)
+				oneMinRate := m[0].ProcessingRates["1m"]
+				rates = append(rates, oneMinRate)
+			}
+			if !ratesMatch(rates) {
+				time.Sleep(waitInterval)
+			} else {
+				succeedChan <- struct{}{}
+				break
+			}
+		}
+	}()
+	select {
+	case <-succeedChan:
+		time.Sleep(waitInterval)
+		break
+	case <-timer.C:
+		assert.Fail(s.T(), "timed out waiting for processing rate to match across vertices.")
+	}
+	timer.Stop()
+}
+
+func ratesMatch(rates []float64) bool {
+	if len(rates) <= 1 {
+		return true
+	}
+	firstVal := rates[0]
+	for i := 1; i < len(rates); i++ {
+		diff := math.Abs(firstVal - rates[i])
+		if diff > (firstVal * 0.1) {
+			return false
+		}
+	}
+	return true
 }
 
 func TestUserDefinedSourceSuite(t *testing.T) {
